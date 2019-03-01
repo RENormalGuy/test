73ccf572a6994c98ed20bb9ae9b7517f57b9101e
RENormalGuy <48140341+RENormalGuy@users.noreply.github.com>
2019-03-02 00:14:22 +0100

    Add files via upload

---
+++ TestConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace TestConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
